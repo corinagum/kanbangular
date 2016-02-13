7cdf80a86033450fe770f960f36e73947bf2c15a
Corina <corina.aj@gmail.com>
2016-02-13 12:07:51 -1000

    modify models to incorporate heroku

--- models/index.js
+++ models/index.js
@@ -8,21 +8,41 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
+
+
+// if(process.env.HEROKU_POSTGRESQL_CHARCOAL_URL ) {
+//   var sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_CHARCOAL_URL, {
+//     dialect : 'postgres',
+//     protocol: 'postgres',
+//     port : match[4],
+//     host : match[3],
+//     logging : true
+//     });
+// }
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+ var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+ var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+ .readdirSync(__dirname)
+ .filter(function(file) {
+   return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+ })
+ .forEach(function(file) {
+   var model = sequelize['import'](path.join(__dirname, file));
+   db[model.name] = model;
+ });
+
+
+
+// global.db = {
+//   Sequelize : Sequelize,
+//   sequelize : sequelize,
+//   User : sequelize.import(__dirname, '/user'),
+//   Task : sequelize.import(__dirname, '/task')
+// };
 
 Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
