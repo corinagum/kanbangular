b325ac4593756933ec8f300f163b523d61d297a2
Corina <corina.aj@gmail.com>
2016-03-16 10:08:57 -1000

    add seeder files and add more material design css

---
+++ seeders/tasks-seed.js
@@ -0,0 +1,28 @@
+// var db = require('./../models');
+
+// module.exports = {
+//   up: function (queryInterface, Sequelize) {
+
+//      // var tasks = require('./../www/assets/bike-rack-locations.js').features;
+//      // var toInsert = [];
+//      // for(var i=0; i<bikeRacks.length; i++){
+//      //  bikeRacks[i].properties.type = 'bikeRack';
+//      //  bikeRacks[i].properties.long = Number(bikeRacks[i].properties.Location.split(',').pop());
+//      //  bikeRacks[i].properties.lat = Number(bikeRacks[i].properties.Location.split(',')[0]);
+//      //  delete bikeRacks[i].properties.Location;
+//      //  toInsert.push(bikeRacks[i].properties);
+//      // }
+//      // return Point.bulkCreate(toInsert);
+//    },
+
+//   down: function (queryInterface, Sequelize) {
+
+//       // Add reverting commands here.
+//       // Return a promise to correctly handle asynchronicity.
+
+//       // Example:
+//       return queryInterface.bulkDelete('Person', null, {});
+
+//   }
+
+// };
